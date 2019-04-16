88e8182e86972454096c4959155f428dca5ac1d3
OfficialBbass <ajokanola881@stu.ui.edu.ng>
2019-04-16 10:09:18 +0100

    new

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
