d1e09b9782ad563bcddaf8d0a3ac52651ccf6412
OfficialBbass <ajokanola881@stu.ui.edu.ng>
2019-04-16 18:28:11 +0100

    updated

--- js/init.js
+++ js/init.js
@@ -5,6 +5,10 @@
     $('.parallax').parallax();
     $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+ 
+    $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
       });
